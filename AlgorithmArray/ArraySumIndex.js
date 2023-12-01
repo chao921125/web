fcb44add12988154aa05c8f94a542db2cd8f5574
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-12-01 17:27:45 +0800

    add

---
+++ AlgorithmArray/ArraySumIndex.js
@@ -0,0 +1,29 @@
+let div = document.createElement("div");
+div.style.color = "#000";
+div.style.backgroundColor = "#fff";
+div.innerHTML += "[1,2,3,4,1,3] 4" + "<br/>";
+
+  function sumIndexBasic(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+div.innerHTML += "Basic: " + sumIndexBasic([1,2,3,4,1,3], 4) + "<br/>";
+
+function sumIndex(array, target) {
+  let hm = new Map();
+  for (let i = 0; i < array.length; i++) {
+    let l = array[i];
+    if (hm.has(target - l))
+      return [hm.get(target - l), i];
+    hm.set(l, i);
+  }
+}
+console.log();
+
+div.innerHTML += "Up: " + sumIndex([1,2,3,4,1,3], 4) + "<br/>";
+document.body.appendChild(div);
---
+++ AlgorithmArray/Test.js
@@ -0,0 +1,5 @@
+let div = document.createElement("div");
+div.style.color = "#000";
+div.style.backgroundColor = "#fff";
+div.innerHTML = "";
+document.body.appendChild(div);
